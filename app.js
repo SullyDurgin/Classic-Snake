7628ba0bb28e962549e49ac6616400e9f70db8b9
SullyDurgin <katsdurgin@gmail.com>
2021-10-11 17:16:21 -0400

    write our pseudocode

---
+++ app.js
@@ -0,0 +1,49 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+/*---------------------------- letiables (state) ----------------------------*/
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+//create/mark the game board (as a grid) in HTML (how many boxes will there be?)
+//create game board somehow (do I do height & width?)
+
+//each cell of the grid should have it's own div
+//20x20 grid = 400 cells
+
+//css boxes on the game board
+
+//loop that keeps track of time & when it is time for the next step (either game over or add a link to the snake)
+
+//handle user input (figure out how to attach a keystroke to an event)
+// document.addEventListener('keydown', (event) => {
+//   var name = event.key;
+//   var code = event.code;
+
+//calculate next position of snake & add to snake
+
+
+//variables
+// board, square on board??
+
+//create board
+//function init()
+
+//game state
+//let snake, snakeDirection, snakeLength
+
+//game loop- to render snake on board throughout game
+
+
+//function rat()
+//math.random for the rat that the snake eats
