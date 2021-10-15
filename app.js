40d9079958f0dc15a5996cf13fc21fe7e85bf3e3
SullyDurgin <katsdurgin@gmail.com>
2021-10-15 15:55:00 -0400

    build js game board

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 /*-------------------------------- Constants --------------------------------*/
 
 
-/*---------------------------- letiables (state) ----------------------------*/
 
+/*---------------------------- letiables (state) ----------------------------*/
+let snake
+let rat
+let snakePosition
+let ratPosition
+
+//game board size (number of cells in grid)
+//20x20 gameBoard = 400 cells
+let width = 20 //cells
+let height = 20 //cells
 
 /*------------------------ Cached Element References ------------------------*/
 
+const gameBoard = document.querySelector('.game-board')
+const cells = document.querySelectorAll(".game-board .cell .inside")
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -14,36 +25,19 @@
 
 /*-------------------------------- Functions --------------------------------*/
 
-
-//create/mark the game board (as a grid) in HTML (how many boxes will there be?)
-//create game board somehow (do I do height & width?)
-
-//each cell of the grid should have it's own div
-//20x20 grid = 400 cells
-
-//css boxes on the game board
-
-//loop that keeps track of time & when it is time for the next step (either game over or add a link to the snake)
-
-//handle user input (figure out how to attach a keystroke to an event)
-// document.addEventListener('keydown', (event) => {
-//   var name = event.key;
-//   var code = event.code;
-
-//calculate next position of snake & add to snake
+//first define the required variables to track game state
+//store cached elements
+//define required constants
 
 
-//variables
-// board, square on board??
+//build the grid for the game board in JS
+for (let i = 0; i < width * height; i++) {
+	const inside = document.createElement('div')
+	inside.setAttribute('class', 'inside')
 
-//create board
-//function init()
+	const cell = document.createElement('div')
+	cell.setAttribute('class', 'cell')
+	cell.appendChild(inside)
 
-//game state
-//let snake, snakeDirection, snakeLength
-
-//game loop- to render snake on board throughout game
-
-
-//function rat()
-//math.random for the rat that the snake eats
+	gameBoard.appendChild(cell)
+}
